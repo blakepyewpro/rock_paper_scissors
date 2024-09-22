fd794c215a9088b36f198bc11b204cf498724f40
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-09-21 21:34:48 -0400

    stubbed out basic structure

---
+++ script.js
@@ -0,0 +1,19 @@
+//1 == Rock, 2 == Paper, 3 == Scissors
+
+function playGame() {
+    //TODO
+}
+
+function playRound(){
+    //TODO
+}
+
+function getComputerChoice() {
+    //generate a random number between 1 and 3
+    let choice = Math.floor(Math.random() * 3) + 1;
+    return choice;
+}
+
+function getPlayerChoice() {
+    //TODO
+}
