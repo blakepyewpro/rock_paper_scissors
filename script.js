6605e489d14b14ffb6025e3f03ee623432bd2b3e
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-09-21 21:52:14 -0400

    added getWinner logic

--- script.js
+++ script.js
@@ -9,11 +9,48 @@ function playRound(){
 }
 
 function getComputerChoice() {
-    //generate a random number between 1 and 3
     let choice = Math.floor(Math.random() * 3) + 1;
     return choice;
 }
 
 function getPlayerChoice() {
-    //TODO
+    let input = prompt(`Please enter "rock", "paper", or "scissors`);
+    switch(input) {
+        case "rock":
+            return 1;
+        case "paper":
+            return 2;
+        case "scissors":
+            return 3;
+        default:
+            getPlayerChoice();
+    }
+}
+
+function getWinner(playerChoice, computerChoice) {
+    if (playerChoice == 1) {
+        if (computerChoice == 1) {
+            return "tie";
+        } else if (computerChoice == 2) {
+            return "computer";
+        } else if (computerChoice == 3) {
+            return "player";
+        }
+    } else if (playerChoice == 2) {
+        if (computerChoice == 1) {
+            return "player";
+        } else if (computerChoice == 2) {
+            return "tie";
+        } else if (computerChoice == 3) {
+            return "computer";
+        }
+    } else if (playerChoice == 3) {
+        if (computerChoice == 1) {
+            return "computer";
+        } else if (computerChoice == 2) {
+            return "player";
+        } else if (computerChoice == 3) {
+            return "tie";
+        }
+    }
 }
